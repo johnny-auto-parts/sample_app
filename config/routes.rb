274c5ed577e82acc52de7b5d4fb3f85690c22f9f
Derek Flionis <dflionis@hotmail.com>
2011-11-30 23:00:55 -0500

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp::Application.routes.draw do
 
+  get "sessions/new" #did i add this? automatic?
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
