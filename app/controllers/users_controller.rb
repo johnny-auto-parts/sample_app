fcf54c9fca8766f44088687f52043f6cba2076ea
Derek Flionis <dflionis@hotmail.com>
2011-12-31 15:48:32 -0500

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update, :destroy]  
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user, :only => :destroy
+  before_filter :exercise, :only => [:new, :create]
 
   def index
     @title = "All users"
@@ -10,6 +11,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end
 
@@ -56,10 +58,6 @@ class UsersController < ApplicationController
 
   private
 
-    def authenticate
-      deny_access unless signed_in?
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
@@ -69,4 +67,7 @@ class UsersController < ApplicationController
       redirect_to(root_path) unless current_user.admin?
     end
   
+    def exercise
+      redirect_to(root_path) if signed_in?
+    end
 end
